f6efbe5a728576887c96bdaf84dc8eab064b7d42
milky_way <125801694+setusq@users.noreply.github.com>
2023-09-08 15:52:34 +0300

    Update simmain.py
    Отступ3

--- simmain.py
+++ simmain.py
@@ -26,6 +26,7 @@ plt.axhline(y=17, color='gray', linestyle='dashed', label='x2=17')
 
 #Вектор Z
 plt.arrow(0, 0, 3, 5, head_width=0.5, head_length=0.7, fc='black', ec='black', label='вектор z (3, 5)')
+
 perpendicular_x = np.array([0, -5])
 perpendicular_y = np.array([0, 3])
 plt.plot(perpendicular_x, perpendicular_y, linestyle='dashed', color='green', label='перпендикуляр')
